5a8f791fec07b1404a3c64eeb1709cee7b6c28d7
cyanjnpr <150724329+cyanjnpr@users.noreply.github.com>
2026-01-12 03:20:09 +0100

    track build number in dev

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,6 +1,7 @@
 export const API_URL = import.meta.env.VITE_CHATTEX_API_URL;
 export const SOCKET_URL = import.meta.env.VITE_CHATTEX_WEBSOCKET_API_URL;
 export const IS_DEV = (import.meta.env.VITE_CHATTEX_DEV) == "true";
+export const BUILD_NUMBER = (IS_DEV ? import.meta.env.VITE_CHATTEX_BUILD_NUMBER : 1);
 
 export function tokenHeader() {
   const t = localStorage.getItem('token');
