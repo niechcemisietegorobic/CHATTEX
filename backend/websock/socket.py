961da18480128e4f0da8b05941aa9ac1093d69c1
cyanjnpr <150724329+cyanjnpr@users.noreply.github.com>
2026-01-14 02:03:35 +0100

    asyncio typo

--- backend/websock/socket.py
+++ backend/websock/socket.py
@@ -19,12 +19,10 @@ def handle_connect(data):
     if (data is not None and data["token"] is not None):
         user_id = socket_auth_user_id(data["token"])
         if (user_id is not None):
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(cache.set(request.sid, user_id))
+            asyncio.run(cache.set(request.sid, user_id))
             socket_sessions[request.sid] = user_id
 
 @socket.on("disconnect")
 def handle_disconnect():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(cache.delete(request.sid))
+    asyncio.run(cache.delete(request.sid))
     del socket_sessions[request.sid]
