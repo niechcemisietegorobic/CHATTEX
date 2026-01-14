cc2db3b51af808444b4207c3eecae311f5572b3e
cyanjnpr <150724329+cyanjnpr@users.noreply.github.com>
2026-01-14 02:22:56 +0100

    further asyncio debugging

--- backend/websock/socket.py
+++ backend/websock/socket.py
@@ -8,6 +8,14 @@ socket = SocketIO(cors_allowed_origins="*")
 
 socket_sessions = {}
 
+def get_or_create_event_loop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        return asyncio.get_event_loop()
+
 def send_to_all_except(user_id: int, event: str, data):
     pass
     
@@ -19,10 +27,12 @@ def handle_connect(data):
     if (data is not None and data["token"] is not None):
         user_id = socket_auth_user_id(data["token"])
         if (user_id is not None):
-            asyncio.run(cache.set(request.sid, user_id))
+            loop = get_or_create_event_loop()
+            loop.run_until_complete(cache.set(request.sid, str(user_id)))
             socket_sessions[request.sid] = user_id
 
 @socket.on("disconnect")
 def handle_disconnect():
-    asyncio.run(cache.delete(request.sid))
+    loop = get_or_create_event_loop()
+    loop.run_until_complete(cache.delete(request.sid))
     del socket_sessions[request.sid]
